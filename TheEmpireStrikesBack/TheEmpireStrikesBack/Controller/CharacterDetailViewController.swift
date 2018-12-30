bac68637146647bc80fac45ffdc69574716f5995
Lauren Small <lauren.small@detroitlabs.com>
2018-12-30 18:11:29 -0500

    create CharacterDetailViewController class, import UIKit so class can extend from UIViewController

---
+++ TheEmpireStrikesBack/TheEmpireStrikesBack/Controller/CharacterDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterDetailViewController.swift
+//  TheEmpireStrikesBack
+//
+//  Created by Lauren Small on 12/30/18.
+//  Copyright © 2018 Lauren Small. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CharacterDetailViewController: UIViewController {
+    
+}
