70638e1d4a9a900bd001b1c2aca8bbe855290e3f
Lauren Small <lauren.small@detroitlabs.com>
2018-12-30 18:42:00 -0500

    create Homeworld model that conforms to Codable protocol

---
+++ TheEmpireStrikesBack/TheEmpireStrikesBack/Model/Homeworld.swift
@@ -0,0 +1,13 @@
+//
+//  Homeworld.swift
+//  TheEmpireStrikesBack
+//
+//  Created by Lauren Small on 12/30/18.
+//  Copyright © 2018 Lauren Small. All rights reserved.
+//
+
+import Foundation
+
+struct Homeworld: Codable {
+    let name: String
+}
